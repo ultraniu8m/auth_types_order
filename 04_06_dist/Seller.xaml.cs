4de91a8534fbccca434b02106b56fcd78beb6ccb
ultraniu8m <art.rumiantsev2016@Yandex.ru>
2022-06-22 14:39:38 +0300

    Добавьте файлы проекта.

---
+++ 04_06_dist/Seller.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using BarcodeLib;
+using iTextSharp;
+using GroupDocs.Signature;
+using GroupDocs.Signature.Options;
+namespace _04_06_dist
+{
+    /// <summary>
+    /// Логика взаимодействия для Seller.xaml
+    /// </summary>
+    public partial class Seller : Page
+    {
+        public Seller()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // данные которые нужно добавить в бд
+            string actual_id = new_order.Text;
+            string actual_client = new_client.Text;
+            string actual_service = new_service.Text;
+
+            /* Создание штрих-кода
+            using (Signature signature = new Signature("path/document.pdf"))
+            {
+                // Создайте параметры штрих-кода с текстом штрих-кода
+                BarcodeSignOptions options = new BarcodeSignOptions("Signed by GroupDocs using GroupDocs.Signature.")
+                {
+                    // Установите тип кодировки штрих-кода
+                    EncodeType = BarcodeTypes.Code128,
+
+                    // Установить положение подписи
+                    Left = 205,
+                    Top = 170,
+                    Width = 200,
+                    Height = 50
+                };
+                // Примените штрих-код к документу для подписи.
+                SignResult result = signature.Sign("path/document-with-barcode.pdf", options);
+            }
+            */
+        }
+    }
+}
---
+++ 04_06_dist/auth.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+namespace _04_06_dist
+{
+    /// <summary>
+    /// Логика взаимодействия для auth.xaml
+    /// </summary>
+    public partial class auth : Page
+    {
+       
+       
+        public auth()
+        {
+            
+            InitializeComponent();
+            
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            string actual_login = login.Text;
+            string actual_password_1 = password_.Password;
+            string actual_password_2 = visible_pass.Text;
+            this.NavigationService.Navigate(new Seller());
+            
+          
+            switch (actual_login)
+            {
+                case "Admin":
+                {
+                        if (actual_password_1 == "Admin" || actual_password_2 == "Admin")
+
+                            this.NavigationService.Navigate(new Admin());
+                        break;
+                }
+                case "Seller":
+                    {
+                        if (actual_password_1 == "Seller" || actual_password_2 == "Seller")
+                            this.NavigationService.Navigate(new Seller());
+                        break;
+                    }
+                case "StSeller":
+                    {
+                        if (actual_password_1 == "StSeller" || actual_password_2 == "StSeller")
+                            this.NavigationService.Navigate(new StSeller());
+                        break;
+                    }
+
+            }
+            
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var checkBox = sender as CheckBox;
+            if (checkBox.IsChecked.Value)
+            {
+                visible_pass.Text = password_.Password;
+                password_.Visibility = Visibility.Hidden;
+                visible_pass.Visibility = Visibility.Visible;
+
+            }
+            else
+            {
+                password_.Password = visible_pass.Text;
+                visible_pass.Visibility = Visibility.Hidden;
+                password_.Visibility = Visibility.Visible;
+            }
+                
+
+        }
+    }
+}
